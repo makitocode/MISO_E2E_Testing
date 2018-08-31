00e8071ef4c9e9d4945a585c58139b7589c7e038
makito <makitodev@outlook.com>
2018-08-31 01:00:33 -0500

    cypress project works fine

---
+++ cypress/cypress/integration/simple_spec.js
@@ -0,0 +1,77 @@
+describe('Los estudiantes login', function() {
+    it('Sign up exitoso de un usuario', function(){
+        //Crear cuenta nueva
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.contains('Ingresar').click()
+        cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Pepito")
+        cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Perez")
+        cy.get('.cajaSignUp').find('input[name="correo"]').click().type("perez.pepito@uniandes.edu.co")
+        cy.get('.cajaSignUp').find('input[type="checkbox"]').check()
+        cy.get('.cajaSignUp').find('input[name="password"]').click().type("password1234")
+        cy.get('.cajaSignUp').find('select[name="idPrograma"]').select('Maestría en Ingeniería de Software')
+        cy.get('.cajaSignUp').contains('Registrarse').click()
+        cy.contains('Registro exitoso!')
+    })
+    it('visits los estudiantes and successfull at login', function(){
+        //Login correcto
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.contains('Ingresar').click()
+        cy.get('.cajaLogIn').find('input[name="correo"]').click().type("perez.pepito@uniandes.edu.co")
+        cy.get('.cajaLogIn').find('input[name="password"]').click().type("password1234")
+        // cy.get('.cajaLogIn').find('input[name="correo"]').click().type("ing.mago.ramirez@gmail.com")
+        // cy.get('.cajaLogIn').find('input[name="password"]').click().type("password1234")
+        cy.get('.cajaLogIn').contains('Ingresar').click()
+        
+    })
+    it('Visits los estudiantes and fails at login', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        //Lineas nuevas  
+        cy.contains('Ingresar').click()
+        cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail@example.com")
+        cy.get('.cajaLogIn').find('input[name="password"]').click().type("1234")
+        cy.get('.cajaLogIn').contains('Ingresar').click()
+        cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
+    })
+    it('Visits los estudiantes and fails at sign up', function(){
+        //Creación de cuenta con login ya existente
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.contains('Ingresar').click()
+        cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Pepito")
+        cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Perez")
+        cy.get('.cajaSignUp').find('input[name="correo"]').click().type("perez.pepito@uniandes.edu.co")
+        cy.get('.cajaSignUp').find('input[type="checkbox"]').check()
+        cy.get('.cajaSignUp').find('input[name="password"]').click().type("password1234")
+        cy.get('.cajaSignUp').find('select[name="idPrograma"]').select('Maestría en Ingeniería de Software')
+        cy.get('.cajaSignUp').contains('Registrarse').click()
+        cy.contains(`Error: Ya existe un usuario registrado con el correo 'perez.pepito@uniandes.edu.co'`)
+    })
+})
+
+describe('Los estudiantes teacher search', function(){
+    it('Search teacher', function(){
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.get('.Select-control').click()
+        cy.focused().type("Dario Correal", { force: true });
+    })
+})
+describe('Los estudiantes teacher profile', function(){
+    it('Search teacher', function(){
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.get(':nth-child(1) > .profesor > a.jsx-633353764 > :nth-child(2)').click()
+    })
+})
+describe('Los estudiantes teacher subject filter', function(){
+    it('Search teacher', function(){
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.get(':nth-child(1) > .profesor > a.jsx-633353764 > :nth-child(2)').click()
+        cy.get('.materias > :nth-child(1)').find('input[type="checkbox"]').check()
+        //cy.get('input.jsx-3367902293').check()
+    })
+})
